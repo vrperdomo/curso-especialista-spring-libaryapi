26502c4b880b042505237a84cdb7cdda4dbbdc01
Victor Perdomo <victor.rodriguesp@gmail.com>
2026-03-13 12:13:23 -0300

    first commit

---
+++ src/main/java/io/github/vrperdomo/libaryapi/LibaryapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.vrperdomo.libaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vrperdomo/libaryapi/LibaryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vrperdomo.libaryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
