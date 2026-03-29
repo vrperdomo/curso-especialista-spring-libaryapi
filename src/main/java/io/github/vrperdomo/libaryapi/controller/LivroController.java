ccdd00911620da1109dfa53ae7e81eec835278c8
Victor Perdomo <victor.rodriguesp@gmail.com>
2026-03-28 22:52:31 -0300

    Implementação do CadastroLivroDTO

--- src/main/java/io/github/vrperdomo/libaryapi/controller/LivroController.java
+++ src/main/java/io/github/vrperdomo/libaryapi/controller/LivroController.java
@@ -1,14 +1,43 @@
 package io.github.vrperdomo.libaryapi.controller;
 
+import io.github.vrperdomo.libaryapi.controller.dto.CadastroLivroDTO;
+import io.github.vrperdomo.libaryapi.controller.dto.ErroRespostaDTO;
+import io.github.vrperdomo.libaryapi.exceptions.RegistroDuplicadoException;
 import io.github.vrperdomo.libaryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("livros")
+@RequestMapping("/livros")
 @RequiredArgsConstructor
 public class LivroController {
 
     private final LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO cadastroLivroDTO){
+
+        try {
+
+            // mapear dto para entidade
+
+            // enviar a entidade para o service validar e salvar na basa
+
+            // criar url para acesso dos dados do livro
+
+            // retornar código created com header location
+
+            return null;
+
+        } catch (RegistroDuplicadoException e){
+            var erroDTO = ErroRespostaDTO.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+
+    }
 }
--- src/main/java/io/github/vrperdomo/libaryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/vrperdomo/libaryapi/controller/dto/CadastroLivroDTO.java
@@ -1,16 +1,32 @@
 package io.github.vrperdomo.libaryapi.controller.dto;
 
 import io.github.vrperdomo.libaryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
+
+        @ISBN
+        @NotBlank(message = "Campo obrigatório")
         String isbn,
-        String Titulo,
+
+        @NotBlank(message = "Campo obrigatório")
+        String titulo,
+
+        @NotNull(message = "Campo obrigatório")
+        @Past(message = "A data de nascimento deve ser uma data anterior à data atual")
         LocalDate dataPublicacao,
+
         GeneroLivro generoLivro,
+
         BigDecimal preco,
+
+        @NotNull(message = "Campo obrigatório")
         UUID idAutor) {
 }
